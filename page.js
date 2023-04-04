ea1c38ee984037218c187d7ddd2f26b277afe9e9
Va13ntin <13valender@gmail.com>
2023-04-04 09:20:38 +0200

    Limit charactère

--- page.js
+++ page.js
@@ -1,7 +1,13 @@
 function showAlert(event) {
-          event.preventDefault();
-          var name = document.getElementById("name").value;
-          var email = document.getElementById("email").value;
-          var message = document.getElementById("message").value;
-          alert("Nom: " + name + "\nEmail: " + email + "\nMessage: " + message);
-        };
+  event.preventDefault();
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  alert("Nom: " + name + "\nEmail: " + email + "\nMessage: " + message);
+}
+
+function checkMessageLength(textarea, maxLength) {
+  if (textarea.value.length > maxLength) {
+    textarea.value = textarea.value.substring(0, maxLength);
+  }
+}
